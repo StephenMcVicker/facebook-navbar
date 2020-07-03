f5d3c77748769e61f8f205274b7d5bc43304fcfe
NeoDragon <mcvickerstephen@gmail.com>
2020-07-03 11:26:40 +0100

    Updates

---
+++ precache-manifest.3aed7ca32760e981da53ecb4735511d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08feda3d9dbf6d84023dcd9ade6b4ad",
+    "url": "/facebook-navbar/index.html"
+  },
+  {
+    "revision": "e93a801caa32114a4269",
+    "url": "/facebook-navbar/static/css/main.addc93ca.chunk.css"
+  },
+  {
+    "revision": "6f84cc342472515ef19d",
+    "url": "/facebook-navbar/static/js/2.13bb1690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facebook-navbar/static/js/2.13bb1690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93a801caa32114a4269",
+    "url": "/facebook-navbar/static/js/main.e96117ae.chunk.js"
+  },
+  {
+    "revision": "1287b13bf8923447422a",
+    "url": "/facebook-navbar/static/js/runtime-main.bcb3b4f5.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/facebook-navbar/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/facebook-navbar/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/facebook-navbar/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/facebook-navbar/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/facebook-navbar/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/facebook-navbar/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/facebook-navbar/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/facebook-navbar/static/media/plus.9311af2e.svg"
+  }
+]);
